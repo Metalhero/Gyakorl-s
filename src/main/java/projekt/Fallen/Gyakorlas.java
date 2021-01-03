e07e66f8ca98de042faeeb9b06fec75c32d2ab81
jaffaszorp <mjozsef16@gmail.com>
2021-01-03 11:18:53 +0100

    Közben csináltam válaszokat

--- src/main/java/projekt/Fallen/Gyakorlas.java
+++ src/main/java/projekt/Fallen/Gyakorlas.java
@@ -1,4 +1,18 @@
 package projekt.Fallen;
 
 public class Gyakorlas {
+        System.out.println("első válasz: Az objektum orintált nyelv a valós világon alapul.Adatokat tartlmaz és kérésre válaszol." +
+                "A legtöbb objektumorientált nyelv osztály alapú, azaz az objektumok osztályok példányai, és típusuk az osztály." +
+                "Egységbezárás: Az egységbe zárás azt fejezi ki, hogy az összetartozó adatok és függvények, eljárások együtt vannak, egy egységbe tartoznak." +
+                "Öröklődés: Majdnem minden osztály alapú nyelv támogatja az öröklődést,Több programozási nyelv is megengedi a többszörös öröklődést, azonban a felülírással együtt ez a káró problémához vezet" +
+                "Az absztrakt osztályok nem példányosíthatók közvetlenül, hanem csak közvetve, konkrét leszármazottaik által." +
+                "második válasz: Kivételkezelés: elkapás vagy továbbengedés." +
+                "harmadik válasz: Filereader,FileWriter" +
+                "3. a. válasz: Az objektumok írását és olvasását nevezzük szerializációnak." +
+                "3. b. válasz: Visszaállítás" +
+                "negyedik válasz: Runnable és Thread" +
+                "ötödik válasz: A rekurziónál nem kell egyik ciklust sem használnod, ami a Java nyelvben elérhető, hisz az utasítások ismételt végrehajtását azzal is elérheted, hogy egy metódust önmagából újra meghívsz" +
+                "hatodik válasz:" +
+                "hetedik válasz: A Set egy speciális Collection, amely nem tartalmaz duplikált elemeket")
+
 }
