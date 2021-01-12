1219703258ca1505e0471a55881be4c4d7e61194
Metalhero <sl3yerkem@gmail.com>
2021-01-12 18:19:21 +0100

    második alkalom

--- src/main/java/projekt/Masodiknap.java
+++ src/main/java/projekt/Masodiknap.java
@@ -9,7 +9,31 @@ public class Masodiknap {
 
         Ponty gyuri=new Ponty();
 
+        System.out.println(Ponty.nbYear(1500000, 0.25, 1000, 2_000_000));
 
+
+        int[] array={1,2,3,4,};
+        int[] array2=new int[array.length];
+
+        System.arraycopy(array,1,array2,1,2);
+
+        for (int i : array2) {
+            System.out.print(" " + i);
+        }
+
+        System.out.println();
+
+        int a= 11;
+        int b= a >> 3;
+        System.out.println(b);
+
+
+        String st = "fapapucs";
+        System.out.println(st.hashCode());
+        String st2 = "fapapucs";
+        System.out.println(st);
+        st="asd";
+        System.out.println(st2);
     }
 
 }
--- src/main/java/projekt/contributors/Gyuri/Ponty.java
+++ src/main/java/projekt/contributors/Gyuri/Ponty.java
@@ -2,4 +2,41 @@ package projekt.contributors.Gyuri;
 
 public class Ponty {
 
+    public static int nbYear(int p0, double percent, int aug, int p) {
+        int totalPop=p0;
+        int year=0;
+        while (totalPop<=p){
+            totalPop=totalPop+(int)(totalPop*percent*0.01)+aug;
+            year++;
+        }
+        return year;
+    }
+
+    public void válaszokPonty(){
+
+        boolean b=true; //false
+        byte by=100;
+        short sh=1;
+        int i=Integer.MAX_VALUE;
+        long l=2_305_324l;
+        float fl=0.4323f;
+        double d=0.235212412;
+        char ch='a';
+        //+1
+
+        System.out.println("select * from table;"); //read
+        System.out.println("insert into table (table1, table2) values (value1, value2);");
+        System.out.println("update table set column1=valueNew, column2=valueNew where id IN (1,5);"); //update
+        System.out.println("delete from table where id=2"); //delete
+
+        System.out.println("create table namae" +
+                            "(id int auto_increment unique," +
+                            "name varchar(255)," +
+                            "");
+
+
+
+
+
+    }
 }
